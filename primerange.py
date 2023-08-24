867a1bc820dd4c1ee23efbca2c42e6801654632e
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2023-08-24 22:19:31 +0530

    primerange

---
+++ primerange.py
@@ -0,0 +1,16 @@
+print("Example range of numbers: a to b")
+a=(int)(input("Enter value of a: "))
+b=(int)(input("Enter value of b: "))
+
+prime= False
+
+for i in range(a,b):
+    if(i>1):
+        prime= True
+    for j in range(2,int(i ** 0.5)+1):
+        if(i%j==0):
+            prime= False
+            break
+    
+    if(prime):
+        print(i,end=" ")
